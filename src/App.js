255b3a623d8fa85a0d51ff02befbfc6a58bcfffe
TamannaSheme <syedatsheme03@gmail.com>
2022-06-07 01:03:46 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import StarRating from "./StarRating";
+
+function App() {
+  return (
+    <div className="App">
+      <StarRating></StarRating>
+    </div>
+  );
+}
+
+export default App;
